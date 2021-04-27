df8d13d3e2741d0cbdc0828f7fc0c30273ff7c89
Maximiliano Usich <maximilianou@gmail.com>
2021-04-27 16:30:56 -0300

    wip(app): looking for api running

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-app.get('/api/prod', (req, res) => {
+const admin = require('firebase-admin');
+const app = admin.initializeApp();
+
+//const express = require('express')
+//const app = express()
+app.get('/api', (req, res) => {
   res.json({ message: `Products API ${(new Date()).toISOString()}`})
 })
 const PORT = process.env.PORT || 7000  
-app.listen( PORT, () => console.log(`Listening in http://localhost:${PORT}/api/prod`))
+app.listen( PORT, () => console.log(`Listening in http://localhost:${PORT}/api`))
